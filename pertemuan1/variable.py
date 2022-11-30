b321975d760f34aecb0261c180c2919917428c70
aldimdiansyah <aldimdiansyah@gmail.com>
2022-11-30 18:16:25 +0700

    praktikum pertama ddp

---
+++ pertemuan1/variable.py
@@ -0,0 +1,16 @@
+# Tipe data di python:
+# 1. String
+# 2. Boolean
+# 3. Float
+# 4. Integer
+# 5. Hexadecimal
+# 6. Dll.
+
+nama = "Aldi" # string
+umur = 20 # integer
+isStudent = True # boolean
+ip = 3.50 # float
+
+Umur20thnMendatang = umur + 20 # operasi aritmatika hanya dapat dilakukan dengan numeric (int/float)
+
+print("Nama saya", nama, '. Umur saya 20 tahun mendatang adalah', Umur20thnMendatang)
